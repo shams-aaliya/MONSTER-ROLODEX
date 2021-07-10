90d10d613446db4853d03ef0090e869191652b45
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-10 14:15:16 +0530

    Committed successfully

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      monsters : [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Banshee',
+          id: '2'
+        },
+        {
+          name: 'Shaggy',
+          id: '3'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
