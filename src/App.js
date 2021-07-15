4068faba962bfdc593a9734c3cbdb902922f1656
shams-aaliya <shamsaaliya08@gmail.com>
2021-07-15 16:30:19 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     
     return (
       <div className="App">
-     
+      <h1>Monsters Rolodex</h1>
       <SearchBox 
       placeholder="Search Monsters"
       handleChange={e => this.setState({ searchField : e.target.value})}
